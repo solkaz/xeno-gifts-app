2219f0480fabf6a99f504e0737ca3ba7a7f98f3b
Jeff Held <jh.software.developer@gmail.com>
2020-09-09 21:10:29 +0200

    Move css imports to src/css.js

---
+++ src/css.js
@@ -0,0 +1,2 @@
+import 'bulma/css/bulma.css';
+import './main.css';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import 'bulma/css/bulma.css';
-import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
 
 Elm.Main.init({
-  node: document.getElementById('root')
+  node: document.getElementById('root'),
 });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+import './css';
