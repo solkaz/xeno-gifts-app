9f01d94815bc56df3cd0643227ca943420e4886a
Jeff Held <jh.software.developer@gmail.com>
2020-09-19 10:44:11 +0200

    Add some info to PageFooter.elm

--- src/PageFooter.elm
+++ src/PageFooter.elm
@@ -1,7 +1,7 @@
 module PageFooter exposing (view)
 
-import Html exposing (Html, div, footer, p, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, a, div, footer, p, text)
+import Html.Attributes exposing (class, href)
 import Msg exposing (Msg)
 
 
@@ -11,5 +11,18 @@ view =
         [ class "footer" ]
         [ div
             [ class "container has-text-centered" ]
-            [ p [] [ text "By Jeff Held" ] ]
+            [ p
+                []
+                [ text "Built with data from "
+                , a [ href "https://xenoblade.fandom.com/wiki/Presents" ] [ text "Xenoblade Chronicles Wikia" ]
+                ]
+            , p []
+                []
+            , p
+                []
+                [ a [ href "https://github.com/solkaz/xenogifts" ] [ text "Source Code" ]
+                , text " by "
+                , a [ href "https://github.com/solkaz" ] [ text "Jeff Held" ]
+                ]
+            ]
         ]
