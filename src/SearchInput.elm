3ce7b747ced575e558c2c3ce95057982770efb38
Jeff Held <jh.software.developer@gmail.com>
2020-09-19 10:15:24 +0200

    Refactor an import in src/SearchInput.elm

--- src/SearchInput.elm
+++ src/SearchInput.elm
@@ -1,12 +1,12 @@
 module SearchInput exposing (view)
 
-import Html exposing (input)
+import Html exposing (Html, input)
 import Html.Attributes exposing (class, placeholder, type_)
 import Html.Events exposing (onInput)
 import Msg exposing (Msg)
 
 
-view : Html.Html Msg
+view : Html Msg
 view =
     input
         [ class "input mb-4"
