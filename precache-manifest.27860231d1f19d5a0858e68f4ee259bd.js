5b6e076e34eb3431d0f7e4bab57aadb9e8110a72
solkaz <jheld135@gmail.com>
2020-09-30 18:00:01 +0000

    Deploying to gh-pages from  @ 64dc5e608d3bf9ad809b0ef1972d9b19a3e56e26 🚀

---
+++ precache-manifest.27860231d1f19d5a0858e68f4ee259bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "8795ee9146ad2868e03475e78a303c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b34ec85800d29736cdb8",
+    "url": "/static/css/main.cbc5fb94.chunk.css"
+  },
+  {
+    "revision": "0ad94c3f8a8457ba87f6",
+    "url": "/static/css/vendors~main.0553ccc1.chunk.css"
+  },
+  {
+    "revision": "b34ec85800d29736cdb8",
+    "url": "/static/js/main.b34ec858.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0ad94c3f8a8457ba87f6",
+    "url": "/static/js/vendors~main.0ad94c3f.chunk.js"
+  }
+]);
